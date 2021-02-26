f39b2b4d79f4410a0d86f93875045c4df6478fb7
yunanlu0404 <luyunan0404@163.com>
2021-02-26 16:56:45 +0800

    cleancode modified

--- Hyper_tuner/testdemo/io/io_continuum.c
+++ Hyper_tuner/testdemo/io/io_continuum.c
@@ -12,11 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ *   IO写文件， IO writes files
+ *   gcc -g io_continuum.c -o io_continuum
  */
-/*
-    IO写文件， IO writes files
-    gcc -g io_continuum.c -o io_continuum
-*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -29,23 +29,23 @@
 
 #define WRITE_BUF_SIZE 64
 #define WRITE_COUNTS 100*1024*1024
-#define INT int 
+#define INT int
 #define CHAR char
 #define VOID void
+#define WRITE_LEVEL 0777
 #define IO_CONTINUUM_TEXT "./io_continuum_test.txt"
 
-enum IO_Operation
-{
+enum IO_Operation {
     Sync_Write = 0,
     Nosync_Write,
 };
 
 INT remove_test_file()
 {
-    if (remove(IO_CONTINUUM_TEXT) != 0)
-    {
+    if (remove(IO_CONTINUUM_TEXT) != 0) {
         printf("Failed to remove file: %s, please delete the file.\n", IO_CONTINUUM_TEXT);
-    }else{
+    }
+    else {
         printf("Success to remove file: %s\n", IO_CONTINUUM_TEXT);
     }
     return 0;
@@ -56,12 +56,11 @@ INT IO_Write_With_Sync()
     INT count = 0;
     INT fd;
     CHAR buf = "test io continuum!\n";
-    if ((fd=open(IO_CONTINUUM_TEXT, O_WRONLY|O_TRUNC|O_CREAT, 0777)) == -1)
-    {
+    if ((fd = open(IO_CONTINUUM_TEXT, O_WRONLY | O_TRUNC | O_CREAT, WRITE_LEVEL)) == -1) {
         return -1;
-    }else{
-        while (count <= WRITE_COUNTS)
-        {
+    }
+    else {
+        while (count <= WRITE_COUNTS) {
             write(fd, buf, WRITE_COUNTS);
             fsync(fd);
             count++;
@@ -75,12 +74,11 @@ INT IO_Write_Without_Sync()
     INT count = 0;
     INT fd;
     CHAR buf = "test io continuum!\n";
-    if ((fd=open(IO_CONTINUUM_TEXT, O_WRONLY|O_TRUNC|O_CREAT, 0777)) == -1)
-    {
+    if ((fd = open(IO_CONTINUUM_TEXT, O_WRONLY | O_TRUNC | O_CREAT, WRITE_LEVEL)) == -1) {
         return -1;
-    }else{
-        while (count <= WRITE_COUNTS)
-        {
+    }
+    else {
+        while (count <= WRITE_COUNTS) {
             write(fd, buf, WRITE_COUNTS);
             count++;
         }
@@ -99,17 +97,17 @@ INT main(INT argc, CHAR **argv)
     INT val;
     val = atoi(argv[1]);
     signal(SIGINT, sighandler);
-    switch (val)
-    {
-        case Sync_Write:
-            IO_Write_With_Sync();
-            remove_test_file();
-            break;
-        case Nosync_Write:
-            IO_Write_Without_Sync();
-            remove_test_file();
-        default:
-            break;
+    switch (val) {
+    case Sync_Write:
+        IO_Write_With_Sync();
+        remove_test_file();
+        break;
+    case Nosync_Write:
+        IO_Write_Without_Sync();
+        remove_test_file();
+        break;
+    default:
+        break;
     }
     return 0;
 }
