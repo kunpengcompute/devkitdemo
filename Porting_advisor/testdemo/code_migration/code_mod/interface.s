c392579b4538368a88b7707436342fb10a942172
yunanlu0404 <75987425+yunanlu0404@users.noreply.github.com>
2021-03-23 14:35:34 +0800

    Update interface.s

--- Porting_advisor/testdemo/code_migration/code_mod/interface.s
+++ Porting_advisor/testdemo/code_migration/code_mod/interface.s
@@ -2,38 +2,38 @@
     .file "llvm-link"
     .p2align 4        // -- Begin function sub_0_caller_wrapper
     .type sub_0_caller_wrapper,@function
-sub_0_caller_wrapper:
+sub_0_caller_wrapper:               // @sub_0_caller_wrapper
 // %bb.0
-    add x8, x0, #0xffffffff
+    and x8, x0, #0xffffffff
     str q0, [sp, #-16]!
-    ldq x9, x10, [sp], #16
+    ldp x9, x10, [sp], #16
     add w11, w0, w1
     mov x0, x7
     stp x8, x11, [x6, #-16]
     stp x9, x10, [x6, #-32]
     ret
 .Lfunc_end0:
-    size sub_0_caller_wrapper, .Lfunc_end0-sub_0_caller_wrapper
+    .size sub_0_caller_wrapper, .Lfunc_end0-sub_0_caller_wrapper
                                           // -- End function
-    .global caller
+    .globl caller                         // --Begin function caller
     .p2align 2
     .type caller,@function
-.caller
-// %bb.0
-    stp x22, x21 [sp, #-32]  // 16-byte Folded Spill
-    stp x20m x19 [sp, #16]   // 16-byte Folded Spill
-    mrs x8, TPIDR EL0
-    add x10m x8, :tprel_hi12:simulation_stack
+caller:                                  //@caller
+// %bb.0:
+    stp x22, x21, [sp, #-32]!  // 16-byte Folded Spill
+    stp x20, x19, [sp, #16]   // 16-byte Folded Spill
+    mrs x8, TPIDR_EL0
+    add x10, x8, :tprel_hi12:simulation_stack
     mov w9, #8388480
     add x10,x10, :tprel_lo12_nc:simulation_stack
     add x8, x8, :tprel_hi12:simulation_stack_pointer
     add x9, x10, x9
     adrp x10, _unnamed_1
     add x8, x8, :tprel_lo12_nc:simulation_stack_pointer
-    add x10m x10, :__unnamed_1
+    add x10, x10, :lo12__unnamed_1
     //APP
-    stur x30m [sp, #-16]
-    sub sp, sp, #16
+    stur x30, [sp, #-16]
+    sub sp, sp, #16             // ==16
     mov x21, x8
     mov x22, x9
     ldr x20, [x21]
@@ -47,18 +47,18 @@ sub_0_caller_wrapper:
     sub x20, x22, x20
     str x20, [x21]
     ldr x19, [sp]
-    mov so, x19
-    ldr x39, [sp]
+    mov sp, x19
+    ldr x30, [sp]
     ldur    x0, [sp, #-8]
     ldur    x1, [sp, #-16]
     ldur    q0, [sp, #-32]
-    add sp, sp, #16         //-16
+    add sp, sp, #16         //=16
 
     //NO_APP
     ldp x20, x19, [sp, #16]     // 16-byte Folded Reload
-    ldp x22, x21, [sp], #32     // 32-byte Folded Reload
+    ldp x22, x21, [sp], #32     // 16-byte Folded Reload
     ret
-.Lfunc_end1
+.Lfunc_end1:
     .size   caller, .Lfunc_end1-caller
                                     // -- End function
     .type   __unnamed_1,@object     //@0
@@ -69,7 +69,7 @@ __unnamed_1:
     .size   __unnamed_1, 8
 
     .type   simulation_stack,@object // @simulation_stack
-    .section    .tbss.simulation_stack,"aGwt",@nobits,simulation_stack,condat
+    .section    .tbss.simulation_stack,"aGwt",@nobits,simulation_stack,comdat
     .globl simulation_stack
     .p2align    4
 simulation_stack:
@@ -77,7 +77,7 @@ simulation_stack:
     .size   simulation_stack, 8388608
     
     .type   simulation_stack_pointer,@object // @simulation_stack_pointer
-    .section    .tbss.simulation_stack_pointer,"aGwt",@nobits,simulation_stack_pointer,condat
+    .section    .tbss.simulation_stack_pointer,"aGwt",@nobits,simulation_stack_pointer,comdat
     .globl simulation_stack_pointer
     .p2align    3
 simulation_stack_pointer:
