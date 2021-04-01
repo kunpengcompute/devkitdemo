226ee199986fc27679281e80f63e4385eea6107f
yunanlu0404 <75987425+yunanlu0404@users.noreply.github.com>
2021-04-01 09:27:23 +0800

    Update ksw.h

--- Porting_advisor/testdemo/code_migration/code/ksw.h
+++ Porting_advisor/testdemo/code_migration/code/ksw.h
@@ -17,7 +17,7 @@
 #define __DEMO_H_
 #include <emmintrin.h>
 
-__m128i add_epi(unsigned int data1, unsigned int data2);
-__m128i max_epi(unsigned int data1, unsigned int data2);
+__m256i add_epi(unsigned int data1, unsigned int data2);
+__m256i max_epi(unsigned int data1, unsigned int data2);
 
 #endif
