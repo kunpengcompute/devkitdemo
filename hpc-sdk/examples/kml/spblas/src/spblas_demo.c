784f9e1e863440f0b21b4fd15240b9bf792d7604
AntsCode <AntsCodeCommunity@163.com>
2022-05-19 10:19:30 +0800

    Translate annotation description information.

--- hpc-sdk/examples/kml/spblas/src/spblas_demo.c
+++ hpc-sdk/examples/kml/spblas/src/spblas_demo.c
@@ -46,14 +46,14 @@ void Print_CSR_To_Matrix(int m, float* values, int values_length,
 			break;
 		}
 		float x[m];
-		// 初始化矩阵行数组
+		// Initialize the matrix row array
 		memset(x, 0, m * sizeof(float));
-		// 根据偏移量确认每一行的值范围
+		// Confirm the range of values for each row based on the offset
 		for (int j = row_offsets[i]; j < row_offsets[i + 1]; ++j)
 		{
 			x[column_indices[j]] = values[j];
 		}
-		// 打印矩阵行数组
+		// print matrix row array
 		for (int j = 0; j < m; ++j)
 		{
 			printf("%.6f\t", x[j]);
@@ -65,17 +65,17 @@ void Print_CSR_To_Matrix(int m, float* values, int values_length,
 
 int main()
 {
-	// 使用 Compressed Sparse Row Formate (CSR) 的存储格式来记录稀疏矩阵
-	// 表示矩阵的行数和列数 A(m*m) [定义NNZ (Num-non-zero) 为矩阵A中非0元素的个数]
+	// Record sparse matrices using the 'Compressed Sparse Row Formate (CSR)' storage format
+	// Represents the number of rows and columns of the matrix A(m*m) [Define NNZ (Num-non-zero) as the number of non-zero elements in matrix A]
 	KML_INT m = 4;
-	// 主要用于记录矩阵 A(m*m) 中从左到右从上到下的非零元素(values)的值, 数组的长度为 NNZ [Values 数组]
+	// Mainly used to record the values of non-zero elements (values) from left to right and top to bottom in the matrix A(m*m), the length of the array is NNZ [Values array]
 	float a[9] = { 2.0F, -3.0F, 7.0F, 1.0F, -6.0F, 8.0F, -4.0F, 5.0F, 9.0F };
-	// 表示矩阵 A(m*m) 中非零元素所在的列索引(column indices), 数组的长度为 NNZ [ColumnIndices 数组]
+	// Indicates the column indices of the non-zero elements in the matrix A(m*m), the length of the array is NNZ [ColumnIndices array]
 	KML_INT ja[9] = { 1, 2, 4, 3, 4, 1, 3, 4, 1 };
-	// 表示矩阵 A(m*m) 的偏移量(row offsets), 数组中前m个元素分别代表矩阵 A(m*m) 中每一行的第一个非零元素在Values数组中的下标，最后一项的数值为 NNZ, 数组长度为 m+1 [RowOffsets 数组]
+	// Represents the offset (row offsets) of the matrix A(m*m), the first m elements in the array represent the subscripts of the first non-zero element of each row in the matrix A(m*m) in the Values array, The value of the last item is NNZ, the length of the array is m+1 [RowOffsets array]
 	KML_INT ia[5] = { 1, 4, 6, 9, 10 };
 
-	// 仅由支持矩阵索引从 0 开始的函数使用[符合 C 语言索引规范]
+	// Only used by functions that support 0-based matrix indexing [conforms to C indexing specification]
 	KML_INT scsrgemv_ja[9];
 	KML_INT scsrgemv_ia[5];
 	for (int i = 0; i < 9; ++i)
@@ -87,9 +87,9 @@ int main()
 		scsrgemv_ia[i] = ia[i] - 1;
 	}
 
-	// 向量x的数组
+	// array of vectors x
 	float x[4] = { 1.0F, 3.0F, -2.0F, 5.0F };
-	// 向量y的数组
+	// array of vectors y
 	float y[4] = { -1.0F, 1.0F, 5.0F, 3.0F };
 
 	printf("Input A(4*4):\n");
@@ -99,7 +99,7 @@ int main()
 	printf("Input y:\n");
 	PrintMatrix(1, m, y);
 
-	// [仅支持矩阵索引从 1 开始]
+	// [only supports matrix indices starting from 1]
 	kml_sparse_status_t status_kml_sparse_scsrgemv = kml_sparse_scsrgemv(KML_SPARSE_OPERATION_NON_TRANSPOSE, m, a, ia, ja, x, y);
 	if (status_kml_sparse_scsrgemv == KML_SPARSE_STATUS_SUCCESS)
 	{
@@ -107,7 +107,7 @@ int main()
 		PrintMatrix(1, m, y);
 	}
 
-	// [仅支持矩阵索引从 1 开始]
+	// [only supports matrix indices starting from 1]
 	kml_sparse_status_t status_kml_sparse_scsrsymv = kml_sparse_scsrsymv(KML_SPARSE_FILL_MODE_LOWER, m, a, ia, ja, x, y);
 	if (status_kml_sparse_scsrsymv == KML_SPARSE_STATUS_SUCCESS)
 	{
@@ -115,7 +115,7 @@ int main()
 		PrintMatrix(1, m, y);
 	}
 
-	// [仅支持矩阵索引从 0 开始]
+	// [only supports matrix indices starting from 0]
 	kml_sparse_status_t status_kml_csparse_scsrgemv = kml_csparse_scsrgemv(KML_SPARSE_OPERATION_NON_TRANSPOSE, m, a, scsrgemv_ia, scsrgemv_ja, x, y);
 	if (status_kml_csparse_scsrgemv == KML_SPARSE_STATUS_SUCCESS)
 	{
