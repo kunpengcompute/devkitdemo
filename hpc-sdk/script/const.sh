118fd92bb65f8b49fd8c5ee19ffd751cadb2dc62
AntsCode <AntsCodeCommunity@163.com>
2022-05-25 19:34:11 +0800

    Project refactoring.

--- hpc-sdk/script/const.sh
+++ hpc-sdk/script/const.sh
@@ -14,17 +14,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# 读取配置文件信息作为安装脚本全局变量使用
+# Read the configuration file and use it as the global variable of the installation script
 current_dir=$(cd $(dirname $0); pwd)
-# os支持
+# Supported OSs
 support_os_names=$(sed '/^SUPPORT_OS_NAME=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)
 
 declare -A soft_support_lists=(["openEuler20.03(LTS-SP1)"]="BISHENG GCC KML"
                                 ["openEuler20.03(LTS-SP2)"]="BISHENG GCC KML"
                                 ["openEuler20.03(LTS-SP3)"]="BISHENG GCC KML"
-                                ["KylinLinuxAdvancedServerV10"]="HMPI_GCC HMPI_BISHENG BISHENG GCC"
+                                ["KylinLinuxAdvancedServerV10"]="HMPI-GCC HMPI-BISHENG BISHENG GCC"
                                 ["UnionTechOSServer20"]="BISHENG GCC"
-                                ["CentOSLinuxrelease7.6.1810"]="HMPI_GCC HMPI_BISHENG BISHENG GCC KML"
+                                ["CentOSLinuxrelease7.6.1810"]="HMPI-GCC HMPI-BISHENG BISHENG GCC KML"
                                 ["Ubuntu20.04"]="BISHENG GCC KML"
                                 ["Ubuntu18.04"]="BISHENG GCC KML"
                                 ["KylinLinuxAdvancedServerV10_HMPI_GCC"]="${kylinv10_hmpi_gcc_name}"
@@ -32,30 +32,31 @@ declare -A soft_support_lists=(["openEuler20.03(LTS-SP1)"]="BISHENG GCC KML"
                                 ["CentOSLinuxrelease7.6.1810_HMPI_GCC"]="${centos7_6_linux_hmpi_gcc_name}"
                                 ["CentOSLinuxrelease7.6.1810_HMPI_BISHENG)"]="${centos7_6_hmpi_bisheng_name}"
 )
-# 磁盘最小要求
+# Minimum drive size
 low_space=$(sed '/^LOW_SPACE=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)
-# 数学库名称
+low_pace_package_g=$(sed '/^LOW_SPACE_PACKAGE_G=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)
+# Math library name
 boost_math_name="boostkit-kml"
 boost_math_rpm_name=$(sed '/^LIBRARY_MATH_RPM_NAME=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)
-boost_math_deb_name=$(sed '/^LIBRARY_MATH_RPM_NAME=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)
+boost_math_deb_name=$(sed '/^LIBRARY_MATH_DEB_NAME=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)
 bisheng_compiler_name=$(sed '/^BISHENG_COMPILER_NAME=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)
 gcc_compiler_name=$(sed '/^GCC_COMPILER_NAME=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)
 kylinv10_hmpi_gcc_name=$(sed '/^KYLINV10_LINUX_HMPI_GCC_NAME=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)
 kylinv10_hmpi_bisheng_name=$(sed '/^KYLINV10_LINUX_HMPI_BISHENG_NAME=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)
 centos7_6_linux_hmpi_gcc_name=$(sed '/^CENTOS7.6_LINUX_HMPI_GCC_NAME=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)
 centos7_6_hmpi_bisheng_name=$(sed '/^CENTOS7.6_LINUX_HMPI_BISHENG_NAME=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)
-# 需要安装的软件总表
+# List of software to be installed
 software_to_be_installed=$(sed '/^SOFTWARE_TO_BE_INSTALLED=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)
-# 自定义路径规则
+# User-defined path rule
 ALLOW_STRING=$(sed '/^ALLOW_STRING=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)
-# 成功或者失败的状态
+# Success or failure status
 SUCCESS=1
 FAILED=0
-# 终端输入的颜色处理
+# Color processing for terminal output
 TIP_COLOR_FAILED=$(sed '/^TIP_COLOR_FAILED=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)       # 红色
 TIP_COLOR_SUCCESS=$(sed '/^TIP_COLOR_SUCCESS=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)     # 绿色
 TIP_COLOR_WARNING=$(sed '/^TIP_COLOR_WARNING=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)     # 黄色
-TIP_COLOR_OSCHECK=$(sed '/^TIP_COLOR_OSCHECK=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)     # 紫色
+TIP_COLOR_COMMAND=$(sed '/^TIP_COLOR_COMMAND=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)     # 紫色
 TIP_COLOR_CHECKING=$(sed '/^TIP_COLOR_CHECKING=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)   # 蓝色
 TIP_COLOR_ECHO=$(sed '/^TIP_COLOR_ECHO=/!d;s/.*=//;s/\r//' ${current_dir}/const.conf)           # 白色
 
