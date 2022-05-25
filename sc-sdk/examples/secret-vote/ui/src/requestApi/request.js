8716c80c7b1e642c291d411c4109d348c01fe302
miaojingjie <miaojingjie@huawei.com>
2022-05-25 21:02:59 +0800

    add exception feedback; add voted status

--- sc-sdk/examples/secret-vote/ui/src/requestApi/request.js
+++ sc-sdk/examples/secret-vote/ui/src/requestApi/request.js
@@ -4,7 +4,7 @@ import { getToken, removeToken, removeUserInfo } from '../utils/auth'
 import { Message } from 'element-ui';
 
 const axiosRequest = axios.create({
-    timeout: 5000
+    timeout: 15 * 1000 // 单位ms
 });
 
 axiosRequest.interceptors.request.use(config => {
@@ -19,27 +19,60 @@ axiosRequest.interceptors.request.use(config => {
 axiosRequest.interceptors.response.use(response => {
     return response;
 }, error => {
-    const { status } = error.response;
-    if (status === 403) {
-        return Promise.reject(error);
-    } else if (status === 401) {
-        removeToken();
-        removeUserInfo();
-        router.push('/login');
+    if (error.response) {
+        const { status } = error.response;
+        if (status === 403) {
+            return Promise.reject(error);
+        } else if (status === 401) {
+            removeToken();
+            removeUserInfo();
+            router.push('/login');
+        } else {
+            removeToken();
+            removeUserInfo();
+            router.push('/login');
+            Message({
+                type: 'error',
+                showClose: true,
+                duration: 0,
+                message: '未知错误',
+                offset: 84
+            })
+        }
     } else {
-        removeToken();
-        removeUserInfo();
-        router.push('/login');
+        dealWithFailOrCancel(error);
+    }
+    return Promise.reject(error);
+})
+
+function dealWithFailOrCancel(error) {
+    const errMessage = error.message;
+    if (errMessage.includes('Network Error')) {
         Message({
             type: 'error',
             showClose: true,
             duration: 0,
-            message: '未知错误',
+            message: '网络错误',
+            offset: 84
+        })
+    } else if (errMessage.includes('timeout')) {
+        Message({
+            type: 'error',
+            showClose: true,
+            duration: 0,
+            message: '请求超时',
+            offset: 84
+        })
+    } else {
+        Message({
+            type: 'error',
+            showClose: true,
+            duration: 0,
+            message: errMessage,
             offset: 84
         })
     }
-    return Promise.reject(error);
-})
+}
 
 // 登录
 export function doLogin(data) {
