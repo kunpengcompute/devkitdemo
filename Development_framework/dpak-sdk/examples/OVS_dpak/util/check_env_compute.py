4aa679f181bb6bbf237cea4ff85c2982c46b1a87
goatmiemie <13041298066@163.com>
2022-09-19 18:44:03 +0800

    update output infomation  delete redundant information

--- Development_framework/dpak-sdk/examples/OVS_dpak/util/check_env_compute.py
+++ Development_framework/dpak-sdk/examples/OVS_dpak/util/check_env_compute.py
@@ -7,7 +7,7 @@ from check_env_remote import CheckDemoEnvironment
 
 def check_compute_server(ip, username, port):
     compute = CheckDemoEnvironment(ip, username, port, 'remote')
-    status, result = compute.check_vm_ping(eth0_ip)
+    status, result = compute.check_vm_ping(eth0_ip, show_message)
 
     return status, result
 
@@ -22,12 +22,14 @@ if __name__ == "__main__":
         username_compute = cfg['compute_first'].get('username_compute_first')
         ip_compute = cfg['compute_first'].get('ip_compute_first')
         eth0_ip = cfg.get('compute_second','eth0_ip_second')
+        show_message = "VM1 can ping vm2"
     else:
         port_compute = cfg['compute_second'].get('port_compute_second')
         username_compute = cfg['compute_second'].get('username_compute_second')
         ip_compute = cfg['compute_second'].get('ip_compute_second')
         eth0_ip = cfg.get('compute_first','eth0_ip_first')
-    status, result = check_compute_server(ip_compute,username_compute,port_compute)
+        show_message = "VM1 can ping vm2"
+    status, result = check_compute_server(ip_compute,username_compute, port_compute)
     if status:
         sys.exit(0)
     sys.exit(1)
