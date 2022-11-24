b8928989489693923831ef741c952d6aa8620080
qinyinw <m15180433431@163.com>
2022-11-24 11:57:35 +0800

    锁竞争调优demo样例修改2

--- Hyper_tuner/testdemo/基于java性能分析工具的锁竞争定位调优实践/Lock.java
+++ Hyper_tuner/testdemo/基于java性能分析工具的锁竞争定位调优实践/Lock.java
@@ -2,23 +2,28 @@ public class Lock {
     private static Lock lock = new Lock();
 
     public static void main(String[] args) throws Exception {
-        while (true) {
-            for (int i = 0; i < 10; i++) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        synchronized (lock) {  
-                            try{
-								Thread.sleep(1000L); 
-							}catch(Exception e){
-								e.printStackTrace();
-							}					
-                          
-                        }
-                    }
-                }).start();
-            }
-            Thread.sleep(3000L);
-        }
+        Thread t = new Thread(new Runnable(){
+			@Override
+			public void run(){
+				synchronized (lock){
+					try{
+						Thread.sleep(10000000L);
+					}catch(Exception e){
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		t.start();
+		for(int i=0;i<50;i++){
+			new Thread(new Runnable(){
+			@Override
+			public void run(){
+				synchronized (lock){
+				    System.out.println("等");
+				}
+			}
+		}).start();
+		}
     }
 }
