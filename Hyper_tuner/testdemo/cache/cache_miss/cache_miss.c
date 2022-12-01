a9f44d46a378f4d03a047afbad07b804c2d534b7
qinyinw <m15180433431@163.com>
2022-12-01 15:46:14 +0800

    update cache folder demo

--- Hyper_tuner/testdemo/cache/cache_miss/cache_miss.c
+++ Hyper_tuner/testdemo/cache/cache_miss/cache_miss.c
@@ -1,20 +1,4 @@
 /*
- * Copyright 2020 Huawei Technologies Co., Ltd
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
 数组访问， Cache Miss
 gcc -g cache_miss.c -o cache_miss
 */
@@ -23,8 +7,8 @@ gcc -g cache_miss.c -o cache_miss
 #include <stdlib.h>
 #include <sys/time.h>
 
-#define ROW (10 * 1024)
-#define COL (10 * 1024)
+#define ROW (20 * 1024)
+#define COL (20 * 1024)
 #define N   (ROW * COL)
 
 
@@ -32,18 +16,14 @@ int main(void)
 {
     int i, j;
     int *p = (int*)malloc(sizeof(int) * N);
-    if (p == NULL) {
-        perror("Memory allocation through malloc failed");
-        exit(EXIT_FAILURE);
-    }
-    double msecs;
+    double msecs =0;
     struct timeval before, after;
 
     gettimeofday(&before, NULL);
 
-    for (i = 0; i < ROW; i++) {
-        for (j = 0; j < COL; j++) {
-            p[i + j * COL] = 'A';
+    for (i = 0; i < COL; i++) {
+        for (j = 0; j < ROW; j++) {
+            p[i + j * COL]++;
         }
     }
 
@@ -53,6 +33,5 @@ int main(void)
     msecs = (after.tv_sec - before.tv_sec) * 1000.0 + (after.tv_usec - before.tv_usec) / 1000.0;
     printf("Execution time = %2.3lf ms\n", msecs);
 
-    free(p);
     return 0;
 }
