c16171bee394d95e8f2d751b1a2433c128c968ba
zhuchao <1970478819@qq.com>
2022-12-05 18:36:46 +0800

    add omp-mpi demo

---
+++ Hyper_tuner/testdemo/omp-mpi/memory_bound.c
@@ -0,0 +1,41 @@
+#include "utils.h"
+
+#define MAX_ARRAY_SIZE     409600
+#define LOOP_SIZE          1
+#define BLOCK_SIZE         4096
+
+void MemoryBoundBench(Point *pointA, double *ret, int n)
+{
+    if (pointA == NULL || ret == NULL) {
+        return;
+    }
+    
+    int i,j;
+    #pragma omp parallel for
+    for (i = 0; i< n; i++) {
+        ret[i] = 0.0;
+        for (j = 0; j < n; j++){
+             ret[i] += pointA[i].x + pointA[j].y;
+        }
+    }
+}
+
+int main()
+{
+   Point *pointA = InitPointsVector(MAX_ARRAY_SIZE);
+   
+   double *ret = (double*)malloc(MAX_ARRAY_SIZE * sizeof(double));
+   if (ret == NULL) {
+      printf("ERROR: Memory Allocate Faild!\n");
+      exit(1);  
+   }
+   
+   uint64_t ts_start = GetTime_ns();
+   int i;
+   for (i = 0; i < LOOP_SIZE; i++) {
+       MemoryBoundBench(pointA, ret, MAX_ARRAY_SIZE);
+   }
+   uint64_t ts_end = GetTime_ns();
+   printf("Total Cost Time = %f ms\n", (ts_end - ts_start) / 1000000.0)
+   return 0;
+}
---
+++ Hyper_tuner/testdemo/omp-mpi/utils.h
@@ -0,0 +1,105 @@
+#ifndef HPC_UTILS_H
+#define HPC_UTILS_H
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <math.h>
+#include <unistd.h>
+#include <assert.h>
+#include <time.h>
+#include <omp.h>
+#include <sched.h>
+#include <string.h>
+
+#define SEC2NS 1000000000
+
+typedef struct Point_t {
+	double x;
+        double y;
+}Point;
+
+static inline uint64_t GetTime_ns()
+{
+  struct timespec ts;
+  clock_gettime(CLOCK_REALTIME, &ts);
+  return ts.tv_sec * SEC2NS + ts.tv_nsec;
+}
+
+static inline uint64_t xorshf96()
+{
+        static uint64_t x=123456789;
+    static uint64_t y=362436069;
+    static uint64_t z=521288629;
+        uint64_t t;
+     
+        x ^= x << 16;
+        x ^= x >> 5;
+        x ^= x << 1;
+  
+        t = x;
+        x = y;
+        y = z;
+
+        z = t ^ x ^ y;
+        return z;
+}
+
+static inline uint64_t my_random()
+{
+        return xorshf96();
+}
+
+static inline double random_double(uint32_t range)
+{
+    return (my_random() % range) * (double)(1.0);
+}
+
+static inline Point *InitPointsVector(int len)
+{
+    Point *pointsVector = (Point*)malloc(len * sizeof(Point));
+    if (pointsVector == NULL) {
+        printf("ERROR: Memory Allocate Failed!\n");
+        exit(1);
+    }
+    int i;
+    #pragma omp parallel for
+    for (i = 0; i < len; i++){
+        pointsVector[i].x = random_double(len);
+        pointsVector[i].y = random_double(len);
+    }
+    return pointsVector;
+}
+
+static inline double ComputPointDistance(Point *pointA, Point *pointB)
+{
+    double horizontal, vertical, pointDis;
+    horizontal = pointA->x - pointB->x;
+    vertical = pointA->y - pointB->y;
+    pointDis = sqrt((horizontal * horizontal) + (vertical * vertical));
+    return pointDis;
+}
+
+void set_thread_affinity(uint16_t core_id)
+{
+    cpu_set_t cpu_mask;
+        cpu_set_t get;
+      
+        CPU_ZERO(&cpu_mask);
+        CPU_SET(core_id, &cpu_mask);
+
+        if(sched_setaffinity(0, sizeof(cpu_mask), &cpu_mask)==-1)
+        {
+          printf("set cpu affinity faild ...\n");
+          exit(1);
+        }
+       
+        if(sched_getaffinity(0, sizeof(get), &get)==-1)
+        {
+           printf("get affinity faild...\n");
+           exit(1);
+        }
+}
+
+#endif
