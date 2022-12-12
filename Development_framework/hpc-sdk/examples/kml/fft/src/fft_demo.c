afb68c982af1574f07c6c2ba3e0e6ea767317f3c
Coolmjt <maojiangtao@huawei.com>
2022-12-12 19:39:20 +0800

    修改FFT Demo单精度版本接口与双精度版本不同

--- Development_framework/hpc-sdk/examples/kml/fft/src/fft_demo.c
+++ Development_framework/hpc-sdk/examples/kml/fft/src/fft_demo.c
@@ -23,7 +23,31 @@
 #include <kfft.h>
 #include <mpi.h>
 
-void PrintComplex(kml_fft_complex* complex, const int length)
+#ifdef SINGLE
+#define FFT_COMPLEX kml_fftf_complex
+#define FFT_PLAN kml_fftf_plan
+#define FFT_MALLOC kml_fftf_malloc
+#define FFT_PLAN_DFT kml_fftf_plan_dft
+#define FFT_PLAN_DFT_1D kml_fftf_plan_dft_1d
+#define FFT_PLAN_DFT_2D kml_fftf_plan_dft_2d
+#define FFT_PLAN_DFT_3D kml_fftf_plan_dft_3d
+#define FFT_DESTROY_PLAN kml_fftf_destroy_plan
+#define FFT_FREE kml_fftf_free
+#define FFT_EXECUTE_DFT kml_fftf_execute_dft
+#else
+#define FFT_COMPLEX kml_fft_complex
+#define FFT_PLAN kml_fft_plan
+#define FFT_MALLOC kml_fft_malloc
+#define FFT_PLAN_DFT kml_fft_plan_dft
+#define FFT_PLAN_DFT_1D kml_fft_plan_dft_1d
+#define FFT_PLAN_DFT_2D kml_fft_plan_dft_2d
+#define FFT_PLAN_DFT_3D kml_fft_plan_dft_3d
+#define FFT_DESTROY_PLAN kml_fft_destroy_plan
+#define FFT_FREE kml_fft_free
+#define FFT_EXECUTE_DFT kml_fft_execute_dft
+#endif
+
+void PrintComplex(FFT_COMPLEX* complex, const int length)
 {
 	printf("{\n");
 	int i;
@@ -48,13 +72,13 @@ int main()
 	// Indicates an array whose dimension is rank, including the size of each dimension in the FFT sequence. 
 	// The constraint is n[i] ≥ 1, for i in 0 to rank - 1.
 	int* n;
-	n = (int*)kml_fft_malloc(sizeof(int) * rank);
+	n = (int*)FFT_MALLOC(sizeof(int) * rank);
 	n[0] = 2;
 	n[1] = 3;
 	double init[6][2] = {{ 120, 0 }, { 8, 8 }, { 0, 0 }, { 0, 16 }, { 0, 16 }, { -8, 8 }};
 
-	kml_fft_complex* in;
-	in = (kml_fft_complex*)kml_fft_malloc(sizeof(kml_fft_complex) * n[0] * n[1]);
+	FFT_COMPLEX* in;
+	in = (FFT_COMPLEX*)FFT_MALLOC(sizeof(FFT_COMPLEX) * n[0] * n[1]);
 	int i;
 	for (i = 0; i < n[0] * n[1]; i++)
 	{
@@ -65,50 +89,50 @@ int main()
 	printf("Input complex:\n");
 	PrintComplex(in, n[0] * n[1]);
 
-	kml_fft_complex* out;
-	out = (kml_fft_complex*)kml_fft_malloc(sizeof(kml_fft_complex) * n[0] * n[1]);
-	kml_fft_plan plan;
+	FFT_COMPLEX* out;
+	out = (FFT_COMPLEX*)FFT_MALLOC(sizeof(FFT_COMPLEX) * n[0] * n[1]);
+	FFT_PLAN plan;
 
 	/*
 	 * Create a plan for the n-dimensional complex-to-complex (C2C) transform of a single contiguous data sequence.
 	 */
-	plan = kml_fft_plan_dft(rank, n, in, out, KML_FFT_FORWARD, KML_FFT_ESTIMATE);
-	kml_fft_execute_dft(plan, in, out);
+	plan = FFT_PLAN_DFT(rank, n, in, out, KML_FFT_FORWARD, KML_FFT_ESTIMATE);
+	FFT_EXECUTE_DFT(plan, in, out);
 
-	printf("kml_fft_plan_dft --> OutPut complex:\n");
+	printf("FFT_PLAN_DFT --> OutPut complex:\n");
 	PrintComplex(out, n[0] * n[1]);
 
 	/*
 	 * Create a plan for the one-dimensional C2C transform of a single contiguous data sequence.
 	 */
-	plan = kml_fft_plan_dft_1d(n[0], in, out, KML_FFT_FORWARD, KML_FFT_ESTIMATE);
-	kml_fft_execute_dft(plan, in, out);
+	plan = FFT_PLAN_DFT_1D(n[0], in, out, KML_FFT_FORWARD, KML_FFT_ESTIMATE);
+	FFT_EXECUTE_DFT(plan, in, out);
 
-	printf("kml_fft_plan_dft_1d --> OutPut complex:\n");
+	printf("FFT_PLAN_DFT_1D --> OutPut complex:\n");
 	PrintComplex(out, n[0] * n[1]);
 
 	/*
 	 * Create a plan for the two-dimensional C2C transform of a single contiguous data sequence.
 	 */
-	plan = kml_fft_plan_dft_2d(n[0], n[1], in, out, KML_FFT_FORWARD, KML_FFT_ESTIMATE);
-	kml_fft_execute_dft(plan, in, out);
+	plan = FFT_PLAN_DFT_2D(n[0], n[1], in, out, KML_FFT_FORWARD, KML_FFT_ESTIMATE);
+	FFT_EXECUTE_DFT(plan, in, out);
 
-	printf("kml_fft_plan_dft_2d --> OutPut complex:\n");
+	printf("FFT_PLAN_DFT_2D --> OutPut complex:\n");
 	PrintComplex(out, n[0] * n[1]);
 
 	/*
 	 * Create a plan for the there-dimensional C2C transform of a single contiguous data sequence.
 	 */
-	plan = kml_fft_plan_dft_3d(n[0], n[1], 1, in, out, KML_FFT_FORWARD, KML_FFT_ESTIMATE);
-	kml_fft_execute_dft(plan, in, out);
+	plan = FFT_PLAN_DFT_3D(n[0], n[1], 1, in, out, KML_FFT_FORWARD, KML_FFT_ESTIMATE);
+	FFT_EXECUTE_DFT(plan, in, out);
 
-	printf("kml_fft_plan_dft_3d --> OutPut complex:\n");
+	printf("FFT_PLAN_DFT_3D --> OutPut complex:\n");
 	PrintComplex(out, n[0] * n[1]);
 
-	kml_fft_destroy_plan(plan);
-	kml_fft_free(n);
-	kml_fft_free(in);
-	kml_fft_free(out);
+	FFT_DESTROY_PLAN(plan);
+	FFT_FREE(n);
+	FFT_FREE(in);
+	FFT_FREE(out);
 	MPI_Finalize();
 	return EXIT_SUCCESS;
 }
