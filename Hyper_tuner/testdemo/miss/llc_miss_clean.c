963d68475a76e9d982fcdcdcf4d6f7871285ddba
qinyinw <m15180433431@163.com>
2022-12-13 15:06:31 +0800

    update miss folder demo

--- Hyper_tuner/testdemo/miss/llc_miss_clean.c
+++ Hyper_tuner/testdemo/miss/llc_miss_clean.c
@@ -4,7 +4,8 @@ gcc -g llc_miss.c -o llc_miss
 #include <stdio.h>
 #include <malloc.h>
 
-void llcMiss3Array(int *** pArr, int x, int y, int z, int m);
+void Malloc3Array(int *** pArr, int x, int y, int z);
+void Free3Array(int *** pArr, int x, int y);
 
 int main(void)
 {
@@ -14,23 +15,36 @@ int main(void)
 	y = 100*1024;
 	z = 100*1024;
 	m = 5*1024;
-	llcMiss3Array(array, x, y, z, m);
+	Malloc3Array(array, x, y, z);
+	Free3Array(array,x,y);
 	array = NULL;
 	return 0;
 }
-void llcMiss3Array(int *** pArr, int x, int y, int z, int m){
+void Malloc3Array(int *** pArr, int x, int y, int z){
     int i, j, k;
-    pArr = (int ***)malloc(m * sizeof(int **));
+    pArr = (int ***)malloc(y * sizeof(int **));
     for(i=0;i<x;i++){
         pArr[i] = (int **)malloc(y*sizeof(int **));
         for (j =0;j<y;j++){
             pArr[i][j] = (int *)malloc(y * sizeof(int));
             for(k =0; k<z; k++){
-                pArr[i][j][k] = 100*(i+j+k+1)+1000*x+100*y+10*z+101*m+19999999999;
+                pArr[i][j][k] = i+j+k+1;
                 printf("%d",pArr[i][j][k]);
             }
             printf("\n");
         }
     printf("\n");
     }
+}
+void Free3Array(int *** pArr, int x, int y){
+    int i, j;
+    for (i=0; i<x;i++){
+        for (j=0;j<y;j++){
+            free(pArr[i][j]);
+            pArr[i][j]=0;
+        }
+        free(pArr[i]);
+        pArr[i]=0;
+    }
+    free(pArr);
 }
