439ccc8f716776c59ab8da14ee847723a04e56c7
qinyinw <m15180433431@163.com>
2023-02-24 09:42:18 +0800

    fix demo

--- Hyper_tuner/testdemo/Know-how/send_recv.cpp
+++ Hyper_tuner/testdemo/Know-how/send_recv.cpp
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 
-void main(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
 	int numprocs, myid, source;
 	MPI_Status status;
+	MPI_Request request;
 	char message[100];
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
@@ -14,22 +15,17 @@ void main(int argc, char* argv[])
 		if (myid != 0) {
 			strcpy(message, "Hello World!");
 			MPI_Send(message, strlen(message) + 1; MPI_CHAR, 0, 99, MPI_COMM_WORLD);
+			//MPI_Isend(message, strlen(message) + 1; MPI_CHAR, 0, 99, MPI_COMM_WORLD, &request);
 		} else {
 			for (source = 1; source < numprocs; source++) {
 				MPI_Recv(message, 100, MPI_CHAR, source, 99, MPI_COMM_WORLD, &status);
+				//MPI_Irecv(message, 100, MPI_CHAR, source, 99, MPI_COMM_WORLD, &request);
 				if (i % 1000000 == 0) {
 					printf("第%d次接收到第%d号进程发送的消息：%s\n", i, source, message);
 				}
 			}
 		}
 	}
-	int data = 100;
-	MPI_Bcast(&data, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	if (rank == 0) {
-		printf("Greeting from master, rank:%d, size:%d\n", rank, p);
-	} else {
-		printf("Greeting from rank:%d, size:%d\n", rank, p);
-	}
-	printf("data:%d in %d rank", data, rank);
 	MPI_Finalize();
+	return 0;
 }
