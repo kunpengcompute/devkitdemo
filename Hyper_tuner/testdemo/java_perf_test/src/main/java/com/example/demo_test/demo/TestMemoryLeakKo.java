659084577399973e3f2b5a0ee5b93563ff94efce
qinyinw <36943426+qinyinw@users.noreply.github.com>
2023-04-24 15:14:19 +0800

    Update TestMemoryLeakKo.java

--- Hyper_tuner/testdemo/java_perf_test/src/main/java/com/example/demo_test/demo/TestMemoryLeakKo.java
+++ Hyper_tuner/testdemo/java_perf_test/src/main/java/com/example/demo_test/demo/TestMemoryLeakKo.java
@@ -5,11 +5,29 @@ public class TestMemoryLeakKo{
       
       public static void main(String[] args){
           boolean flag = true;
-          Thread thread = new Thread(() ->
+          Thread thread = new Thread(() ->{
               while(flag) {
                   List<Person> list = new ArrayList<>(1000);
-                  for (int )
+                  for (int i = 0; i<1000;i++){
+                        list.add(new Person());
+                  }
+                  hashSetPerson.add(list);
+                  try{
+                        Thread.sleep(500L);
+                        if(hashSetPerson.size()>500){
+                              hashSetPerson.clear();
+                        }
+                  }catch (Exception e) {
+                        e.printStackTrace();
+                  }
               }
-          )
+          });
+          thread .start();
+      }
+      
+      public static class Person{
+            String name = "abc";
+            int age = 11;
+            byte[] b = new byte[1024*32];
       }
 }
