697f37ae865a14d2905c6894472707d345096e1c
qinyinw <36943426+qinyinw@users.noreply.github.com>
2023-04-24 16:16:57 +0800

    Update GcTestController.java

--- Hyper_tuner/testdemo/java_perf_test/src/main/java/com/example/demo_test/demo/GcTestController.java
+++ Hyper_tuner/testdemo/java_perf_test/src/main/java/com/example/demo_test/demo/GcTestController.java
@@ -1,6 +1,6 @@
 package com.example.demo_test.demo;
 
-import lombok.extern.slf4j.Slf4j:
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
