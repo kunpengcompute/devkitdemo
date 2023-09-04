0ad940830cfbe3f81bb5afee9ec30f3eb8f72475
--list <AntsCodeCommunity@163.com>
2023-09-05 07:55:45 +0800

    Fix SSL hostname checksums in distributed builds and limit the use of TLSv1.2.

--- devops/jenkins-plugin/porting-advisor-plugin/src/main/java/io/jenkins/plugins/devkit/webserver/APIClient.java
+++ devops/jenkins-plugin/porting-advisor-plugin/src/main/java/io/jenkins/plugins/devkit/webserver/APIClient.java
@@ -1,18 +1,14 @@
 package io.jenkins.plugins.devkit.webserver;
 
 
-import jenkins.security.CustomClassFilter;
-
+import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.DataOutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import java.security.cert.X509Certificate;
-import java.util.Arrays;
-
 
 public class APIClient {
     public static HttpURLConnection httpResponse(String apiUrl, String apiToken, String apiData, String requestMethod) throws Exception {
@@ -21,6 +17,7 @@ public class APIClient {
             public X509Certificate[] getAcceptedIssuers() {
                 return null;
             }
+
             public void checkClientTrusted(X509Certificate[] certs, String authType) {
             }
 
@@ -29,8 +26,9 @@ public class APIClient {
         }};
 
         // Install the all-trusting trust manager
-        SSLContext sslContext = SSLContext.getInstance("SSL");
+        SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
         sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
         HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
 
         // Create connection
@@ -54,37 +52,18 @@ public class APIClient {
 
         return connection;
     }
-
-
     public static Boolean ApiErrCode(String code) {
-        String[] errorCodes = {
-                "0x01011e", "0x010611",
-                "0x010599", "0x0c0310",
-                "0x0c0311", "0x050512",
-                "0x010416", "0x0c0312",
-                "0x050314", "0x010811",
-                "0x050519", "0x0c0314",
-                "0x05051a", "0x0c0315",
-                "0x0c0210", "0x0c0316",
-                "0x05051b", "0x0c0317",
-                "0x0d0110", "0x0c0318",
-                "0x0d0203", "0x0c0211",
-                "0x0d0204", "0x0c0110",
-                "0x0d0205", "0x0d0206",
-                "0x0d0209", "0x0d020b",
-                "0x0d020c", "0x0d020e",
-                "0x0d020f", "0x0d020g",
-                "0x0d0210", "0x0d0211",
-                "0x0d0213", "0x0d0214",
-                "0x0d0215", "0x0d0216",
-                "0x0d0219", "0x0d021a",
-                "0x0d021b", "0x0d021c",
-                "0x0d021e", "0x0d021f",
-                "0x0d0221", "0x0d0223",
-                "0x0d0208", "0x0d0218"
-
-        };
-        for (String codeNum: errorCodes) {
+        String[] errorCodes = {"0x01011e", "0x010611", "0x010599", "0x0c0310",
+                "0x0c0311", "0x050512", "0x010416", "0x0c0312", "0x050314",
+                "0x010811", "0x050519", "0x0c0314", "0x05051a", "0x0c0315",
+                "0x0c0210", "0x0c0316", "0x05051b", "0x0c0317", "0x0d0110",
+                "0x0c0318", "0x0d0203", "0x0c0211", "0x0d0204", "0x0c0110",
+                "0x0d0205", "0x0d0206", "0x0d0209", "0x0d020b", "0x0d020c",
+                "0x0d020e", "0x0d020f", "0x0d020g", "0x0d0210", "0x0d0211",
+                "0x0d0213", "0x0d0214", "0x0d0215", "0x0d0216", "0x0d0219",
+                "0x0d021a", "0x0d021b", "0x0d021c", "0x0d021e", "0x0d021f",
+                "0x0d0221", "0x0d0223", "0x0d0208", "0x0d0218"};
+        for (String codeNum : errorCodes) {
             if (codeNum.equals(code)) {
                 return false;
             }
