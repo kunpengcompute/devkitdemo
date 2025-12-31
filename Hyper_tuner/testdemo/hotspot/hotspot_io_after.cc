1131d86ecec11fe305641707986971b0e0b4c3d1
h30068529 <huangchangjiang2@h-partners.com>
2025-12-31 11:16:35 +0800

    增加热点demo时间戳

--- Hyper_tuner/testdemo/hotspot/hotspot_io_after.cc
+++ Hyper_tuner/testdemo/hotspot/hotspot_io_after.cc
@@ -51,12 +51,19 @@ void process_buffer(char* buffer, size_t file_size) {
 int main(int argc, const char *argv[]) {
     string filename = argv[1];
     int fd;
+    clock_t start = clock();
     auto [buffer, file_size] = map_file_to_buffer(filename, fd);
+    clock_t end = clock();
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    std::cout << "读取数据时间: " << time_spent << " 秒" << std::endl;
+    clock_t start1 = clock();
     if (buffer && file_size > 0) {
         process_buffer(buffer, file_size);
         munmap(buffer, file_size);
         close(fd);
     }
-
+    clock_t end1 = clock();
+    double time_spent1 = (double)(end1 - start1) / CLOCKS_PER_SEC;
+    std::cout << "处理数据时间: " << time_spent1 << " 秒" << std::endl;
     return 0;
 }
--- Hyper_tuner/testdemo/hotspot/hotspot_io_before.cc
+++ Hyper_tuner/testdemo/hotspot/hotspot_io_before.cc
@@ -1,3 +1,4 @@
+#include <ctime>
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -35,8 +36,17 @@ void process_buffer(std::vector<char>& buffer) {
 
 int main(int argc, const char *argv[]) {
     string filename = argv[1];
+    clock_t start = clock();
     auto buffer = read_file_to_buffer(filename);
+    clock_t end = clock();
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    std::cout << "读取数据时间: " << time_spent << " 秒" << std::endl;
+    clock_t start1 = clock();
     process_buffer(buffer);
+    clock_t end1 = clock();
+    double time_spent1 = (double)(end1 - start1) / CLOCKS_PER_SEC;
+    std::cout << "处理数据时间: " << time_spent1 << " 秒" << std::endl;
 
     return 0;
 }
+
